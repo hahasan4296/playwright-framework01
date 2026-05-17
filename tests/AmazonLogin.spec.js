a204a1fb9783dcd96cb504ba8eb4540e1e19b423
Hamza <hasanhamza4296@gmail.com>
2026-05-17 20:17:36 +0530

    Adding file Amazon Login

---
+++ tests/AmazonLogin.spec.js
@@ -0,0 +1,15 @@
+const {test, expect} = require('@playwright/test')
+
+test('Amazon Login', async ({page})=>{
+
+await page.goto("https://www.amazon.in/")
+await page.getByText("Hello, sign in").click()
+await page.locator("#ap_email_login").fill("hamza123@gmail.com")
+await page.getByRole("button", {"name":"Continue"}).click()
+await page.locator("#ap_password").fill("12345")
+await page.getByRole("button", {"name":"Sign in"}).click()
+
+//validation
+await expect(page.getByText("Your password is incorrect")).toContainText('Your password is incorrect')
+
+})
